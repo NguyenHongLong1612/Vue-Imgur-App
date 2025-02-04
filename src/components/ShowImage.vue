78c52a47b2f7ca842fcce0d2f2928e9d9b68dc48
NguyenHongLong1612 <nguyenlong16122003@gmail.com>
2025-02-04 23:36:02 +0700

    fix dynamic show image

--- src/components/ShowImage.vue
+++ src/components/ShowImage.vue
@@ -52,17 +52,22 @@ export default {
 }
 
 .image-container {
-    max-width: 85vw;
-    max-height: 85vh;
+    max-width: 80%;
+    max-height: 100%;
+    position: absolute;
+    top: 0;
+    left: 0;
+    right: 0;
+    bottom: 0;
+    margin: auto;
     display: flex;
     align-items: center;
     justify-content: center;
 }
 
 .image-container img {
-    width: 100%;
-    height: 100%;
-    object-fit: contain;
+    max-height: 100%;
+    max-width: 100%;
 }
 
 .exit {
